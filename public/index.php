29fb8e0007168aaeb77aad899dd3637c3fbf11ab
Stefano <stefano.oldeman@gmail.com>
2010-10-17 13:31:25 +0200

    Added markdown lib

--- public/index.php
+++ public/index.php
@@ -21,6 +21,8 @@ require_once('Dwoo/dwooAutoload.php');
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
+require 'markdown.php';
+
 // Create application, bootstrap, and run
 $application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/config.ini');
 $application->bootstrap()->run();
